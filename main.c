3e4002cf1a1c3871157474ed592cdb309d40910f
Eliot Partridge <shortcircuit369@gmail.com>
2020-02-04 15:03:41 -0600

    Add path builtin

--- main.c
+++ main.c
@@ -8,9 +8,19 @@
 const char ERROR_MESSAGE[30] = "An error has occurred\n";
 
 struct pathentry {
-	const char *entry;
+	char entry[256];
 	struct pathentry *next;
 };
+
+void free_path_list(struct pathentry *head) {
+	struct pathentry *tmp;
+
+	while (head != NULL) {
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
 
 int main(int argc, char *argv[]) {
 	if (argc > 2) {
@@ -19,9 +29,9 @@ int main(int argc, char *argv[]) {
 	}
 
 	struct pathentry *path = malloc(sizeof(struct pathentry));
-	path->entry = "/bin";
+	strcpy(path->entry, "/bin");
 	path->next = malloc(sizeof(struct pathentry));
-	path->next->entry = "/usr/bin";
+	strcpy(path->entry, "/usr/bin");
 	path->next->next = NULL;
 
 	int interactive = 1;
@@ -47,22 +57,37 @@ int main(int argc, char *argv[]) {
 	if (interactive) write(STDOUT_FILENO, "dash> ", 6);
 
 	while ((nRead = getline(&inLine, &inLen, stdin)) != -1) {
+		inLine[strcspn(inLine, "\r\n")] = 0; // strip \r\n
 		char *cmd = strtok(inLine, " ");
-		cmd[strcspn(cmd, "\r\n")] = 0; // strip \r\n
 
 		if (strncmp(cmd, "exit", 4) == 0) {
 			if (strtok(NULL, " ")) {
 				write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
 			} else return EXIT_SUCCESS;
 		} else if (strncmp(cmd, "cd", 2) == 0) {
-			char* path = strtok(NULL,  "");
-			if (!path) {
+			char *cdPath = strtok(NULL, "");
+			if (!cdPath) {
 				write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
 			} else {
-				path[strcspn(path, "\r\n")] = 0; // strip \r\n
-				if (chdir(path) == -1) {
+				if (chdir(cdPath) == -1) {
 					write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
 				}
+			}
+		} else if (strncmp(cmd, "path", 4) == 0) {
+			char *pathDir;
+			free_path_list(path);
+			path = NULL;
+			struct pathentry *tmp = NULL;
+			while ((pathDir = strtok(NULL, " ")) != NULL) {
+				if (path == NULL) {
+					path = malloc(sizeof(struct pathentry));
+					tmp = path;
+				} else {
+					tmp->next = malloc(sizeof(struct pathentry));
+					tmp = tmp->next;
+				}
+
+				strncpy(tmp->entry, pathDir, 256);
 			}
 		} else {
 			char search[512];
@@ -87,7 +112,6 @@ int main(int argc, char *argv[]) {
 				size_t argN = 2;
 				char *arg;
 				while ((arg = strtok(NULL, " ")) != NULL) {
-					arg[strcspn(arg, "\r\n")] = 0;
 					args = realloc(args, ++argN * sizeof(char*));
 					args[argN-2] = arg;
 					args[argN-1] = NULL;
